9500b84186c7fb6d1f3925b6514492291442f5ed
yeizer74 <yeimer.z@outlook.com>
2024-08-22 10:41:13 -0500

    Comando Start

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import app from "./app.js"
 import { PORT } from "./config.js"
 
 
-app.listen (PORT, ()=> console.log ("Servidor inicializado en el puerto "+PORT))
+app.listen (3000, ()=> console.log ("Servidor inicializado en el puerto "+PORT))
